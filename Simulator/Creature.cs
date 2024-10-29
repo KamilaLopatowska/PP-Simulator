0369ec0f7b723b63346e89e676effa069c6470ef
KamilaLopatowska <kamilalopatowska@gmail.com>
2024-10-29 22:57:50 +0100

    Settery

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,23 +1,73 @@
 ﻿namespace Simulator;
+
 public class Creature
 {
-    public string? Name { get; set; }
-    public int Level { get; set; }
+    private string _name = "Unknown";
+    private int _level; 
 
-    public Creature(string name, int level=1)
+    public string Name
     {
-        Name = name;
-        Level = level;
+        get => _name;
+        init
+        {
+            value = value?.Trim() ?? "Unknown";
+
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }
+            else if (value.Length > 25)
+            {
+                value = value.Substring(0, 25).TrimEnd();
+            }
+
+            if (char.IsLower(value[0]))
+            {
+                value = char.ToUpper(value[0]) + value[1..];
+            }
+
+            _name = value; 
+        }
     }
 
-    public Creature()
+    public int Level
     {
-
+        get => _level; 
+        set 
+        {
+            if (value < 1)
+            {
+                _level = 1; 
+            }
+            else if (value > 10)
+            {
+                _level = 10; 
+            }
+            else
+            {
+                _level = value; 
+            }
+        }
     }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name; 
+        Level = level; 
+    }
+
+    public Creature() { }
+
     public void SayHi() =>
         Console.WriteLine($"Hi, I'm {Name} my level is {Level}.");
-    public string Info
+
+    public string Info => $"{Name} [{Level}]";
+
+    public void Upgrade()
     {
-        get{ return $"{Name} [{Level}]"; } 
+        if (Level < 10)
+        {
+            Level++;
+        }
     }
 }
