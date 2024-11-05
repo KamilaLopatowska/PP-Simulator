6906b515d5b40952955b8d6746716da90ebe6b5d
KamilaLopatowska <kamilalopatowska@gmail.com>
2024-11-05 14:10:49 +0100

    Ptaki

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,20 +1,32 @@
 ﻿namespace Simulator;
+
 public class Animals
 {
     private string _description = "Unknown";
 
-    public string? Description
+    public Animals() { }
+
+    public Animals(string description, uint size)
+    {
+        Description = description;
+        Size = size;
+    }
+
+    public string Description
     {
         get => _description;
-        init
+        set
         {
-            Validator.Shortener(_description, 3, 15, '#');
+            _description = Validator.Shortener(value, 3, 15, '#');
         }
     }
+
     public uint Size { get; set; } = 3;
 
-    public string Info
-    { 
+    public virtual string Info
+    {
         get { return $"{Description} <{Size}>"; }
     }
+
+    public override string ToString() => $"{GetType().Name.ToUpper()}: {Info}";
 }
---
+++ Simulator/Birds.cs
@@ -0,0 +1,22 @@
+﻿namespace Simulator;
+
+public class Birds : Animals
+{
+    public bool CanFly { get; set; } = true;
+
+    public Birds() { } 
+
+    public Birds(string description, uint size, bool canFly = true) : base(description, size)
+    {
+        CanFly = canFly;
+    }
+
+    public override string Info
+    {
+        get
+        {
+            string flyAbility = CanFly ? "fly+" : "fly-";
+            return $"{Description} ({flyAbility}) <{Size}>";
+        }
+    }
+}
